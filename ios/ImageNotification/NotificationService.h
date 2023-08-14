1c30b1b942bec4ac4a5ba0c53b647f5ed2a6f392
Cristian Vargas <cristianvargas316@gmail.com>
2023-08-14 19:05:05 -0400

    Se agrega configuracion ios para recibir notificaciones push y local

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Cristian Eduardo Vargas Gallardo on 14-08-23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
